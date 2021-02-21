c724863175543c59d5e4036f2cd4b3e1691ae189
tom lot <tomlot2012@gmail.com>
2021-02-22 01:01:48 +0200

    added tests

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 syncplay/syncplayClient.py --no-gui -n Server -a syncplay.pl:8999 -r 8 --player-path /HttpPlayer
+python3 syncplay/syncplayClient.py --no-gui -n Server -a syncplay.pl:8999 -r 8 --player-path http://127.0.0.1:5000/data
